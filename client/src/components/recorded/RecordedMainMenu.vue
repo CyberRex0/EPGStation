5cf15017e007357946d24f17185b4357e749e137
hr <band3@rakumail.jp>
2026-01-21 16:37:21 +0900

    クリーンアップボタンを削除
    Bye-Bye Cleanup

--- client/src/components/recorded/RecordedMainMenu.vue
+++ client/src/components/recorded/RecordedMainMenu.vue
@@ -16,15 +16,6 @@
                     </v-list-item-content>
                 </v-list-item>
 
-                <v-list-item v-on:click="cleanup">
-                    <v-list-item-icon class="mr-3">
-                        <v-icon>mdi-delete</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>クリーンアップ</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-
                 <v-list-item v-on:click="upload">
                     <v-list-item-icon class="mr-3">
                         <v-icon>mdi-upload</v-icon>
@@ -51,9 +42,6 @@ export default class RecordedMainMenu extends Vue {
         this.$emit('edit');
     }
 
-    public cleanup(): void {
-        this.$emit('cleanup');
-    }
 
     public async upload(): Promise<void> {
         await Util.sleep(200);
